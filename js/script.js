c48466180cfe28ed22a0c1e0d8d79f4df373d341
jasonduquain <jduquain@gmail.com>
2018-01-01 08:32:34 -0500

    added js for sticky hdr and btt btn along with some MQ styles

--- js/script.js
+++ js/script.js
@@ -34,7 +34,41 @@ listItems.forEach((el, i) => {
     });
 });
 
+/************* BACK TO TOP BTN ***************/
 
+let bttBtn = document.querySelector('#back-to-top');
+let docBody = document.body;
+let docElement = document.documentElement;
+let heights = [docBody.offsetHeight, docElement.offsetHeight, docBody.scrollHeight, docElement.scrollHeight];
+let tallest = Math.max(...heights);
+
+let oneThirdDown = tallest / 2;
+
+document.addEventListener('scroll', () => {
+    (docElement.scrollTop >= oneThirdDown) ? bttBtn.classList.add('js--visible') : bttBtn.classList.remove('js--visible');
+});
+
+bttBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    docElement.scrollTop = 0;
+});
+
+/*******  STICKY HDR    **********/
+let hdr = document.querySelector('header');
+let hero = document.querySelector('.hero');
+let hdrHeight = hdr.getBoundingClientRect().height;
+
+
+document.addEventListener('scroll', (e) => {
+    let heroBtm = hero.getBoundingClientRect().bottom;
+    if (heroBtm <= 0) {
+        docBody.style.setProperty('padding-top', `${hdrHeight}px`);
+        hdr.classList.add('js--sticky');
+    } else {
+        hdr.classList.remove('js--sticky');
+        docBody.style.setProperty('padding-top', 0);
+    }
+})
 
 /**********  COPYRIGHT DATE   **********/
 let currentDate =  document.querySelector('.date');
